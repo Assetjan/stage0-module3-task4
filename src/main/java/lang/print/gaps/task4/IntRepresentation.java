bae60bf229ca783cc25733d31d641445c1a1eeb8
Assetjan <asetziyabek@gmail.com>
2023-01-15 02:58:07 +0600

    a

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char r = 99;
+        char a = 104;
+        char h = 97;
+        char c = 114;
+        System.out.print(r);
+        System.out.print(a);
+        System.out.print(h);
+        System.out.print(c);
     }
 }
